f3f31f27f890f769447167a96e674b2ba2645659
Plasmoxy <sebohacker@gmail.com>
2018-02-06 20:28:05 +0100

    asdasd

---
+++ index.js
@@ -0,0 +1,14 @@
+var fag = document.getElementById("fag");
+
+function osc_faggot() {
+  fag.style.color = "blue";
+  fag.innerHTML = "faggot";
+  setTimeout(osc_kys, 1);
+}
+
+function osc_kys() {
+  fag.style.color = "red";
+  fag.innerHTML = "kys";
+}
+
+osc_faggot();
