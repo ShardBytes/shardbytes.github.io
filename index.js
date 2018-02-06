44da8e62907c94180628f535b221c57cbf27676c
Plasmoxy <sebohacker@gmail.com>
2018-02-06 20:32:12 +0100

    dd

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var fag = document.getElementById("fag");
 function osc_faggot() {
   fag.style.color = "blue";
   fag.innerHTML = "faggot";
-  setTimeout(osc_kys, 1);
+  setTimeout(osc_kys, 500);
 }
 
 function osc_kys() {
   fag.style.color = "red";
   fag.innerHTML = "kys";
-  setTimeout(osc_faggot, 1);
+  setTimeout(osc_faggot, 500);
 }
 
-osc_faggot();
+setTimeout(osc_faggot, 500);
