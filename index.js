1561f909d15a4976e81a58a67e8b3a84fc15808c
Plajdo <31418257+Plajdo@users.noreply.github.com>
2018-02-07 15:13:18 +0100

    ahahahaahahaa

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
+var blinktime = 20;
+var fagtime = 500;
+
 var fag = document.getElementById("fag");
+var nigg = document.getElementById("nigg");
+
+var xd = false;
 
 function osc_faggot() {
   fag.style.color = "blue";
   fag.innerHTML = "faggot";
-  setTimeout(osc_kys, 500);
+  setTimeout(osc_kys, fagtime);
 }
 
 function osc_kys() {
   fag.style.color = "red";
   fag.innerHTML = "kys";
-  setTimeout(osc_faggot, 500);
+  setTimeout(osc_faggot, fagtime);
 }
 
-setTimeout(osc_faggot, 500);
+function blincc() {
+  if (xd ) {
+	nigg.style.color = "white";
+	xd = false;
+  } else {
+	nigg.style.color = "black";
+	xd = true;
+  }
+  setTimeout(blincc, blinktime);
+}
+
+setTimeout(osc_faggot, fagtime);
+setTimeout(blincc(), blinktime);
